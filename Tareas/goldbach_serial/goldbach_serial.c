f8648510b9972a17c9a61f26e3d50dabb4b10d15
JAROD.VENEGAS <JAROD.VENEGAS@ucr.ac.cr>
2021-04-14 15:47:20 -0600

    doxygen1

--- Tareas/goldbach_serial/goldbach_serial.c
+++ Tareas/goldbach_serial/goldbach_serial.c
@@ -1,19 +1,18 @@
-#include <stdio.h>
-#include <math.h>
 #include <ctype.h>
-#include <string.h>
+#include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * @brief Determina si un numero es primo
  * @param numero entero
  * @return devuelve un 1 si es primo y un 0 si no es primo
-*/
-int es_primo(long long int num){
-    for(long long int i = 2; i < num/2; i++){
-            
-        if (num % i == 0){
-            return 0; // no es primo
+ */
+int es_primo(int64_t num) {
+    for (int64_t i = 2; i <= num/2 ; i++) {
+        if (num % i == 0) {
+            return 0;  // no es primo
         }
     }
     return 1;  // si es primo
@@ -23,48 +22,46 @@ int es_primo(long long int num){
  * @brief Determina si un numero es par o impar
  * @param numero entero
  * @return devuelve un 1 si es par y un 0 si es impar
-*/
-int par_impar(long long int num)
-{
-    if(num<0){num * -1;}
-    if (num % 2 == 0){
-        return 1; // el numero es par
+ */
+int par_impar(int64_t num) {
+    if (num < 0) {
+        num = num * -1;
     }
-    return 0; // el numero es impar
+    if (num % 2 == 0) {
+        return 1;  // el numero es par
+    }
+    return 0;  // el numero es impar
 }
 
 /**
  * @brief Para todo numero par utiliza la conjetura fuerte de goldbach
  * @param numero entero
  * @return void
-*/
-void conjetura_fuerte(long long int num_temp)
-{
-    for(long long int x=1; x<=2; x++){
-        long long int numero = 0;
-        
-        if(num_temp<0){
-            numero=num_temp * -1;
-        }
-        else{
+ */
+void conjetura_fuerte(int64_t num_temp) {
+    for (int64_t x = 1; x <= 2; x++) {
+        int64_t numero = 0;
+
+        if (num_temp < 0) {
+            numero = num_temp * -1;
+        } else {
             numero = num_temp;
         }
 
-        long long int contador = 0;
-        long long int temp = 2;
+        int64_t contador = 0;
+        int64_t temp = 2;
 
-        for(long long int i = numero-temp; i>2; i--){
-            if (es_primo(temp)==1 && es_primo(i)==1 && temp<=i){
+        for (int64_t i = numero - temp; i > 2; i--) {
+            if (es_primo(temp) == 1 && es_primo(i) == 1 && temp <= i) {
                 contador++;
-                if(x==2 && num_temp<0 )
-                {
-                    printf("%lld + %lld, ",temp,i);
+                if (x == 2 && num_temp < 0) {
+                    printf("%ld + %ld, ", temp, i);
                 }
             }
             temp++;
         }
-        if(x==1){
-            printf ("%lld: %lld sums : ",numero, contador);
+        if (x == 1) {
+            printf("%ld: %ld sums : ", numero, contador);
         }
     }
 }
@@ -73,34 +70,28 @@ void conjetura_fuerte(long long int num_temp)
  * @brief Para todo numero impar utiliza la conjetura fuerte de goldbach
  * @param numero entero
  * @return void
-*/
-void conjetura_debil(long long int num_temp){
+ */
+void conjetura_debil(int64_t num_temp) {
+    for (int64_t i = 1; i <= 2; i++) {
+        int64_t numero = 0;
 
-    for(long long int i=1; i<=2;i++){
-        long long int numero=0;
-
-        if( num_temp<0 ){
-            numero=num_temp * -1;
-        }
-        else{
+        if (num_temp < 0) {
+            numero = num_temp * -1;
+        } else {
             numero = num_temp;
         }
 
-        long long int contador = 0;
+        int64_t contador = 0;
 
-        for ( long long int x = 2; x < numero; x++ ){
-            if ( es_primo(x) == 1 ){
-
-                for ( long long int y = x; y < numero; y++ ){
-                    if ( es_primo(y) == 1 ) {
-
-                        for ( long long int z = y; z < numero; z++ ){
-                            if ( x + y + z == numero && es_primo(z)==1 ){
-
+        for (int64_t x = 2; x < numero; x++) {
+            if (es_primo(x) == 1) {
+                for (int64_t y = x; y < numero; y++) {
+                    if (es_primo(y) == 1) {
+                        for (int64_t z = y; z < numero; z++) {
+                            if (x + y + z == numero && es_primo(z) == 1) {
                                 contador++;
-                                if(i==2 && num_temp<0 ){
-
-                                    printf("%lld + %lld + %lld, ",x, y, z);
+                                if (i == 2 && num_temp < 0) {
+                                    printf("%ld + %ld + %ld, ", x, y, z);
                                 }
                             }
                         }
@@ -108,27 +99,27 @@ void conjetura_debil(long long int num_temp){
                 }
             }
         }
-        if(i==1){
-            printf ("%lld: %lld sums : ",numero, contador);
+        if (i == 1) {
+            printf("%ld: %ld sums : ", numero, contador);
         }
     }
 }
 
 /**
  * @brief Determina si el dato ingresado es un caracter alfabetico
- * @param char 
+ * @param char
  * @return void
-*/
-int es_alpha(char *dato_entrada){
+ */
+int es_alpha(char *dato_entrada) {
     int tamano = strlen(dato_entrada);
     int ptr = 0;
 
-    if(dato_entrada[0] == '-'){
-        ptr=1;
+    if (dato_entrada[0] == '-') {
+        ptr = 1;
     }
 
-    for(int i = ptr; i<tamano; i++){
-        if(isalpha(dato_entrada[i])){
+    for (int i = ptr; i < tamano; i++) {
+        if (isalpha(dato_entrada[i])) {
             return 1;
         }
     }
@@ -136,22 +127,20 @@ int es_alpha(char *dato_entrada){
 }
 
 /**
- * @brief Metodo que controla la entrada de datos para utilizar la conjetura de GoldBach
- * @param numero entero 
+ * @brief Metodo que controla la entrada de datos para utilizar la conjetura de
+ * GoldBach
+ * @param numero entero
  * @return void
-*/
-void goldbach(long long int num){
-    if(num > pow(2, 63)-1 || ( num <= 5 && num >= -5) ){
-        printf ("%lld: NA\n",num);
-    }
-    else{
-
-        if(par_impar(num) == 1){
+ */
+void goldbach(int64_t num) {
+    if (num > pow(2, 63) - 1 || num < -pow(2, 63) - 1 || (num <= 5 && num >= -5)) {
+        printf("%ld: NA\n", num);
+    } else {
+        if (par_impar(num) == 1) {
             conjetura_fuerte(num);
-        }
-        else{
+        } else {
             conjetura_debil(num);
-        }   
+        }
     }
 }
 
@@ -159,26 +148,24 @@ void goldbach(long long int num){
  * @brief Muestra la interfaz del programa
  * @param -
  * @return void
-*/
-void iniciar(){
+ */
+void iniciar() {
     char dato_entrada[100];
     int opcion = 1;
-    while(opcion == 1)
-    {
+
+    while (opcion == 1) {
         printf("\nIngrese un digito mayor de 5 o menor de -5: \nNumero: ");
-        scanf("%p", &dato_entrada);
+        scanf("%s", dato_entrada);
 
-        if(es_alpha(dato_entrada) == 0 ){
-
-            long long int num = atol(dato_entrada);
+        if (es_alpha(dato_entrada)== 0) {
+            int64_t num = atol(dato_entrada);
 
             goldbach(num);
             printf("\n");
             printf("\n\nDesea continuar?   || 1=Si || 0=No ||\n");
             scanf("%d", &opcion);
 
-        }
-        else{
+        } else {
             opcion = 0;
         }
     }
@@ -187,8 +174,7 @@ void iniciar(){
 /**
  * @return El menu del progama de la Conjetura De GoldBach
  */
-int main(void){
-
+int main(void) {
     iniciar();
     return 0;
 }
